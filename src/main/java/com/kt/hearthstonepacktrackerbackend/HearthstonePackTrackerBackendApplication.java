a9cdc1320323b1fba6cd9cec6f18ba714fab8b5b
KT <krakovszkyt@gmail.com>
2024-02-07 21:04:05 +0100

    initial commit

---
+++ src/main/java/com/kt/hearthstonepacktrackerbackend/HearthstonePackTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kt.hearthstonepacktrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearthstonePackTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HearthstonePackTrackerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kt/hearthstonepacktrackerbackend/HearthstonePackTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.hearthstonepacktrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearthstonePackTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
